c522ee030eb56aaaa53ae12a96d4a6593aa102c2
James Chandler <stillsound@protonmail.com>
2021-10-13 17:23:08 -0700

    code model for event in models.py/create directions and corrections in fake_data.sql

--- Nitelife/main_app/models.py
+++ Nitelife/main_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+# class Event(models.Event):
+
+#     creator = models.ForeignKey(User, on_delete=models.CASCADE, serialize=True, related_name='creator')
+#     title = models.CharField(max_length=255, required=True)
+#     location = models.CharField(max_length=255, required=True)
+#     bio = models.TextField(max_length=500, blank=True)
+#     image = models.ImageField()
+#     guest = models.ForeignKey(User, on_delete=models.CASCADE, serialize=True, related_name='guest')
+
+#     def __str__(self):
+#         return self.title
