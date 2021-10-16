10b1cc66e1cb211f76dc75c16bd0a41524fe8280
makmizi <makaiharris@gmail.com>
2021-10-16 11:47:12 -0700

    quick fix

--- Nitelife/Nitelife/settings.py
+++ Nitelife/Nitelife/settings.py
@@ -80,8 +80,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'nitelife',
-        'USER': 'yogesh',
-        'PASSWORD': 'admin',
     }
 }
 
