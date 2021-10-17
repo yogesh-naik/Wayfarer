f8dad523bac310428925502cc96333f36ecf0e8c
James Chandler <stillsound@protonmail.com>
2021-10-16 17:01:37 -0700

    debug urls.py to render views and correct reverse

--- Nitelife/main_app/urls.py
+++ Nitelife/main_app/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('registration/signup/', views.SignUp.as_view(), name="signup"),
     path('profile/', views.ProfileView.as_view(), name="profile"),
     path('events/<int:pk>/', views.EventDetail.as_view(), name="event_detail"),
-    path('events/new', views.CreateEvent.as_view(), name="createevent"),
-    path('events/', views.EventList.as_view(), name="eventlist"),
+    path('events/new', views.CreateEvent.as_view(), name="create_event"),
+    path('events/', views.EventList.as_view(), name="event_list"),
     path('events/<int:pk>/delete', views.EventDelete.as_view(), name='event_delete'),
     path('events/<int:pk>/update', views.EventUpdate.as_view(), name="event_update"),
 ]
