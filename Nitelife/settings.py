37c9d469ee58a864b192c6049b1904f232672ec5
James Chandler <stillsound@protonmail.com>
2021-10-28 22:39:30 -0700

    setting installed apps config

--- Nitelife/settings.py
+++ Nitelife/settings.py
@@ -39,9 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main_app' ,
-    'tailwind',
-    'theme',
     "theme.apps.ThemeConfig",
+    'theme',
+    'tailwind',
     "Nitelife.wsgi"
 ]
 
