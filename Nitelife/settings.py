19a4751cbefbd661d8553f4119903209ccd78744
James Chandler <stillsound@protonmail.com>
2021-10-29 16:41:38 -0700

    settings and base

--- Nitelife/settings.py
+++ Nitelife/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
     }
 }
 
-
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 
@@ -158,8 +158,11 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 
+<<<<<<< HEAD
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 STATIC_ROOT = '../collect_static'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+=======
+>>>>>>> parent of 10f47a6 (intital mig)
