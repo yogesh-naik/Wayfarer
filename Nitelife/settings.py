10f47a65d7640c5c4d504e316f6db38650c4ab8f
James Chandler <stillsound@protonmail.com>
2021-10-29 15:03:45 -0700

    intital mig

--- Nitelife/settings.py
+++ Nitelife/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
     }
 }
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
 
 
 
@@ -157,3 +157,4 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
