aff09ee8020138ed77fd67d3d625eab6ef1bd169
James Chandler <stillsound@protonmail.com>
2021-11-02 20:36:31 -0700

    urls fix

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls import url
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
     path('registration/signup/', views.SignUp.as_view(), name="signup"),
-    path('profile/', views.ProfileView.as_view(), name="profile"),
+    path('profile', views.ProfileView.as_view(), name="profile"),
     path('profile/<int:pk>/', views.ProfileUpdate.as_view(), name="profile_update"),
     path('events/<int:pk>/', views.EventDetail.as_view(), name="event_detail"),
     path('events/new/', views.CreateEvent.as_view(), name="create_event"),
